f5a7cda7000cdd8e1b1125f78f4eb45921a74765
URK21CS3010 <109952119+URK21CS3010@users.noreply.github.com>
2022-10-24 13:56:43 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 class BinaryTreeNode:
     def __init__(self, data):
         self.data = data
-        self.left = None
-        self.right = None
+        self.left_child = None
+        self.right_child = None
 
 
 def insert(root, new_value) -> BinaryTreeNode:
@@ -11,35 +11,43 @@ def insert(root, new_value) -> BinaryTreeNode:
         If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
         Finally, return the root.
         """
-    # Write your code here
-    if root == None:
+    if not root:
         root = BinaryTreeNode(new_value)
-    elif new_value < root.data:
-        root.left = insert(root.left, new_value)
-    else: #new_value >= root.data:
-        root.right = insert(root.right, new_value)
-    return root        
+        return root
+    if new_value < root.data:
+         if root.left_child:
+            insert(root.left_child, new_value)
+         else:
+            root.left_child = BinaryTreeNode(new_value)
+    else:
+        if root.right_child:
+            insert(root.right_child, new_value)
+        else:
+            root.right_child = BinaryTreeNode(new_value)
 
 
 def inorder(root) -> None:
-    if root: 
-        inorder(root.left)
-        print(root.data, end = " ")
-        inorder(root.right)
+    if root is None:
+        return
+    inorder(root.left_child)
+    print(root.data, end = " ")
+    inorder(root.right_child)
 
 
 def preorder(root) -> None:
-    if root:
-        print(root.data, end = " ")
-        preorder(root.left)
-        preorder(root.right)
+    if root is None:
+        return
+    print(root.data, end = " ")
+    preorder(root.left_child)
+    preorder(root.right_child)
 
 
 def postorder(root) -> None:
-    if root:
-        postorder(root.left)
-        postorder(root.right)
-        print(root.data, end = " ")
+    if root is None:
+       return
+    postorder(root.left_child)
+    postorder(root.right_child)
+    print(root.data, end = " ")
 
 
 # Do not change the following code
